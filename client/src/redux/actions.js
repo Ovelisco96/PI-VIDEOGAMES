c3ba8dc2e484f13dcc218b7c46e2e41fab2c133b
Ovelisco96 <123895378+Ovelisco96@users.noreply.github.com>
2023-05-02 17:06:49 -0500

    feat(store): Creacion del store para la aplicacion

---
+++ client/src/redux/actions.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+export const GET_VIDEOGAMES = "GET_VIDEOGAMES";
+export const GET_BY_RATING = "GET_BY_RATING";
+export const GET_BY_ALP = "GET_BY_ALP";
+export const GET_BY_GENRE = "GET_BY_GENRE";
+export const GET_GENRES = "GET_GENRES";
+export const GET_BY_DB = "GET_BY_DB";
+export const SEARCH = "SEARCH";
+export const ERROR = "ERROR";
+export const CLOSE_ERROR = "CLOSE_ERROR";
+export const RESET_FILTERS = "RESET_FILTERS";
+export const GET_PLATFORMS = "GET_PLATFORMS";
+export const CREATE_GAME = "CREATE_GAME";
+
+export const getVideogames = () => async dispatch => {
+    try {
+        console.log("====holiii");
+        let result = await axios.get('http://localhost:3001/videogames')
+        console.log("🚀 ~ file: actions.js:18 ~ getVideogames ~ result:", result)
+        return dispatch({type: GET_VIDEOGAMES, payload: result.data})
+    } catch (error) {
+        return dispatch({type: ERROR, payload: error});
+    }
+};
+
+export const getVideogamesByName = (name) => async dispatch => {
+    try {
+        let result = await axios.get(`http://localhost:3001/videogames?name=${name}`);
+        return dispatch({type: SEARCH, payload: result.data})
+    } catch(error) {
+        return dispatch({type: ERROR, payload: error});
+    };
+};
+
+export const getGenres = () => async dispatch => {
+    let result = await axios.get("http://localhost:3001/genres");
+    return dispatch({type: GET_GENRES, payload: result.data});
+};
+
+export const getPlatforms = () => async dispatch => {
+    let result = await axios.get("http://localhost:3001/platforms");
+    return dispatch({type: GET_PLATFORMS, payload: result.data});
+};
+
+export const getVideogamesByRate = (rate) => dispatch => {
+    return dispatch({type: GET_BY_RATING, payload: rate});
+};
+
+export const getVideogamesByAlp = (alp) => dispatch => {
+    return dispatch({type:GET_BY_ALP, payload: alp});
+};
+
+
+export const getByGenre = (genre) => dispatch => {
+    try {
+        return dispatch({type: GET_BY_GENRE, payload: genre})
+    } catch (error) {
+        return dispatch({type: ERROR, payload: error});
+    }
+};
+
+export const getDbGames = (value) => dispatch => {
+    return dispatch({type: GET_BY_DB, payload: value });
+};
+
+export const createVideogame = (game) => async dispatch => {
+    const newGame = await axios.post("http://localhost:3001/videogames", game);
+    return dispatch({type: CREATE_GAME, payload: newGame.data})
+};
+
+export const resetFilters = () => dispatch => {
+    return dispatch({type: RESET_FILTERS});
+};
+
+export const closeError = () => dispatch =>{
+    return dispatch({type: CLOSE_ERROR});
+}; 
---
+++ client/src/redux/reducer.js
@@ -0,0 +1,129 @@
+import { GET_VIDEOGAMES,
+         SEARCH,
+         ERROR,
+         CLOSE_ERROR,
+         GET_BY_RATING,
+         GET_BY_ALP, 
+         GET_GENRES,
+         GET_BY_GENRE,
+         GET_BY_DB,
+         RESET_FILTERS,
+         GET_PLATFORMS,
+         CREATE_GAME,
+        /*  GET_BY_RATE_P */
+        } from "./actions";
+
+const initialState = {
+    videoGames: [],
+    sortGames: [],
+    genres: [],
+    platforms: [],
+    error: false,
+    errormsg: {}
+};
+
+const rootReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case GET_VIDEOGAMES:
+            const g = [...action.payload]
+            return {
+                ...state,
+                videoGames: action.payload,
+                sortGames: g
+            };
+        case GET_BY_RATING:
+            const gamesSorted = action.payload === "higer"
+                              ? state.sortGames.sort((a, b) => b.rating - a.rating)
+                              : action.payload === "lower"
+                              ? state.sortGames.sort((a,b) => a.rating - b.rating)
+                              : [...state.sortGames];
+            return {
+                ...state,
+                sortGames: gamesSorted
+            };
+        case GET_BY_ALP:
+            const sortByAlp = action.payload === "asc"
+                              ? state.sortGames.sort((a,b) => {if(a.name > b.name) return 1;
+                                                                if(a.name < b.name) return -1;
+                                                                return 0;
+                                                               })
+                              : action.payload === "desc"
+                              ? state.sortGames.sort((a,b) => {if(a.name > b.name) return -1;
+                                                                if(a.name < b.name) return 1;
+                                                                return 0;
+                                                               })
+                              : [...state.sortGames];
+            return {
+                ...state,
+                sortGames: sortByAlp
+            }
+        case GET_GENRES:
+            return {
+                ...state,
+                genres: action.payload
+            }
+        case GET_PLATFORMS:
+            return {
+                ...state,
+                platforms: action.payload
+            };
+        case GET_BY_GENRE:
+            let gamesFilt = state.sortGames.filter(game => game.genres.includes(action.payload));
+            let err = !gamesFilt.length
+            return {
+                ...state,
+                sortGames: gamesFilt,
+                error: err ? !state.error : state.error
+            }
+        case GET_BY_DB:
+            const dbOApi = action.payload === "db" 
+                           ? state.videoGames.filter(game => game.id.toString().includes("-"))
+                           : action.payload === "api"
+                           ? state.videoGames.filter(game => !game.id.toString().includes("-"))
+                           : [...state.videoGames];
+            return {
+                ...state,
+                sortGames: dbOApi
+            };
+        case CREATE_GAME:
+            if(action.payload.status === 200) {
+                return {
+                    ...state,
+                    errormsg: {}
+                } 
+            } else {
+                return {
+                    ...state,
+                    errormsg: action.payload
+                }
+            }
+        case SEARCH:
+            return {
+                ...state,
+                sortGames: action.payload,
+            };
+        case RESET_FILTERS:
+            const reset = state.videoGames;
+            return {
+                ...state,
+                videoGames: state.videoGames,
+                sortGames: reset
+            };
+        case CLOSE_ERROR:
+            return {
+                ...state,
+                error: state.error === false ? false : false,
+            }
+        case ERROR:
+            return {
+                ...state,
+                error: true,
+            }
+        default:
+            return {
+                ...state,
+            };
+    };
+};
+
+export default rootReducer;
